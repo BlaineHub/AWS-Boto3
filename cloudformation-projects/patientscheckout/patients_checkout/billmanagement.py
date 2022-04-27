b1c361f0a0dcf1879ee41838afba88aa07337883
Bod273191 <99423749+Bod273191@users.noreply.github.com>
2022-04-27 16:45:14 +0100

    ''

--- cloudformation-projects/patientscheckout/patients_checkout/billmanagement.py
+++
@@ -1,3 +0,0 @@
-def lambda_handler(event,context):
-    message = event['Records'][0]['Sns']['Message']
-    print(message)
--- cloudformation-projects/patientscheckout/patients_checkout/claimmanagement.py
+++
@@ -1,4 +0,0 @@
-def lambda_handler(event,context):
-    for each_record in event['Records']:
-        print(each_record['body'])
- 
