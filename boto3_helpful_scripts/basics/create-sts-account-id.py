2fd9709df1688c0c553e56b0a93fdcbabee376c0
unknown <blaineodriscoll91@gmail.com>
2023-01-07 22:07:14 +0000

    First Commit

--- boto3_helpful_scripts/basics/create-sts-account-id.py
+++ boto3_helpful_scripts/basics/create-sts-account-id.py
@@ -4,3 +4,5 @@ aws_console = boto3.session.Session(profile_name='developer')
 sts_console = aws_console.client(service_name='sts',region_name='us-east-1')
 
 print(sts_console.get_caller_identity()['Account'])
+
+print('Test Commit')
